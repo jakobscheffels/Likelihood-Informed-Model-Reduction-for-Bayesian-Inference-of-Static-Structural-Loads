2fb3f960b6479162f45a6a877f7747babdd4ea65
jakobscheffels <86796817+jakobscheffels@users.noreply.github.com>
2026-03-11 18:00:01 -0400

    Stochastic load plate

--- Plate.m
+++ Plate.m
@@ -1,5 +1,5 @@
 clear all
-
+rng(42)
 load plate_hole.mat
 
 nodes = size(coordinatesFEM,1);
@@ -8,6 +8,7 @@ ndof = 2*nodes;
 
 Y_bottom = min(coordinatesFEM(:,2));
 tol = 1e-8;
+plate_length = max(coordinatesFEM(:,1))-min(coordinatesFEM(:,1));
 
 bottom_nodes = find(abs(coordinatesFEM(:,2) - Y_bottom) < tol);
 dofs_bottom = zeros(2*size(bottom_nodes,1),1);
@@ -20,27 +21,52 @@ coords_bottom = coordinatesFEM(bottom_nodes,:);
 Y_top = max(coordinatesFEM(:,2));
 top_nodes = find(abs(coordinatesFEM(:,2)-Y_top)<tol);
 dofs_top = zeros(2*size(top_nodes,1),1);
+dofs_topY = zeros(size(top_nodes,1),1);
 for i=1:size(top_nodes,1)
     dofs_top(2*i-1)=2.*top_nodes(i)-1;
     dofs_top(2*i)=2.*top_nodes(i);
+    dofs_topY(i)=2.*top_nodes(i);
 end
 coords_top = coordinatesFEM(top_nodes,:);
 
+
+%% Loading 
+l_c = 0.3*plate_length;
 mu=1e6;
-delta=0.03;
-q=mu;
+delta=0.3;
+
+nSegments = length(top_nodes)-1;
+x_mid = zeros(nSegments,1);
+for i=1:nSegments
+    x_left = coordinatesFEM(top_nodes(i),1);
+    x_right = coordinatesFEM(top_nodes(i+1),1);
+    x_mid(i) = (x_right+x_left)/2;
+end
+sigma = delta*mu;
+C = zeros(nSegments,nSegments);
+for i=1:nSegments
+    for j=1:nSegments
+        C(i,j)=sigma^2*exp(-abs(x_mid(i)-x_mid(j))/l_c);
+    end
+end
+L = chol(C,"lower");
+z=randn(nSegments,1);
+q_realization = mu+L*z;
+
 
 F = zeros(ndof,1);
-F(dofs_top(2:2:end))=q.*(coords_top(2,1)-coords_top(1,1));
-F(dofs_top(2))=F(dofs_top(2))/2;
-F(top_nodes(end))=F(dofs_top(end))/2;
+for i=1:nSegments
+    node_left = top_nodes(i);
+    node_right = top_nodes(i+1);
+    L_seg = coordinatesFEM(node_right,1)-coordinatesFEM(node_left,1);
+    f_local = q_realization(i)*L_seg/2 * [1;1];
 
+    F(dofs_top(2*i))=F(dofs_top(2*i))+f_local(1);
+    F(dofs_top(2*(i+1)))=F(dofs_top(2*(i+1)))+f_local(2);
+end
+% for plotting
 F_full = F;
-%F(bottom_nodes)=[];
 
-%E = 210e9;
-%nu = 0.3;
-%D= E/(1-nu^2)*[1 nu 0; nu 1 0; 0 0 (1-nu)/2];
 
 
 %% --- Load mesh ---
@@ -62,16 +88,6 @@ for i = 1:Nelements
     end
 end
 
-for e = 1:Nelements
-    nodes_e = elementsFEM(e,:);      % 1x4 array of node indices
-    coords_e = coordinatesFEM(nodes_e,:);  % 4x2 coordinates
-    psi = 0; eta = 0;             % center of element
-    J = Jacobian(coords_e(:,1), coords_e(:,2), eta, psi);
-    if det(J) <= 0
-        fprintf('Flipped element %d: det(J) = %f\n', e, det(J));
-    end
-end
-
 %% --- Material properties ---
 E  = 210e9;  % Young's modulus [Pa]
 nu = 0.3;    % Poisson's ratio
@@ -84,6 +100,7 @@ for i=1:Nelements
     K(idx,idx)=K(idx,idx)+Ke;
 end
 
+%% Apply Boundary Condition
 K(dofs_bottom,:)=[];
 K(:,dofs_bottom)=[];
 F(dofs_bottom)=[];
@@ -123,25 +140,7 @@ for e = 1:size(elementsFEM,1)
     dofs = elementDofs(e,:);
     xdofs = dofs(1:2:end);
     ydofs = dofs(2:2:end);
-    %{
-    list = [];
-    listx = [];
-    listy = [];
-    for i=1:4
-        if ismember(idx(i),bottom_nodes)
-            list = [list i];
-        end
-        if ismember(xdofs(i),dofs_bottom)
-            listx = [listx i];
-        end
-        if ismember(ydofs(i),dofs_bottom)
-            listy = [listy i];
-        end
-    end
-    idx(list)=[];
-    xdofs(listx)=[];
-    ydofs(listy)=[];
-    %}
+    
     x = coordinatesFEM(idx,1)+scale.*U_full(xdofs);
     y = coordinatesFEM(idx,2)+scale.*U_full(ydofs);
     
@@ -171,7 +170,7 @@ for e = 1:size(elementsFEM,1)
     plot(x,y,'k')
 end
 for i = 1:size(top_nodes,1)
-    quiver(coords_top(i,1),coords_top(i,2),0,F_full(dofs_top(2*i))./mu*5,"r","LineWidth",1.5)
+    quiver(coords_top(i,1),coords_top(i,2),0,F_full(dofs_topY(i))./mu*5,"r","LineWidth",1.5)
 end
 
 
